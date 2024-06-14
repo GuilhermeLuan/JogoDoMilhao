f19cf6f024b1823b1c85ea5b78d6d09dee228218
Luís Medrado <131623138+LuisMedrado@users.noreply.github.com>
2024-06-14 14:45:39 -0300

    feat: chance extra

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <semaphore.h>
+#include <pthread.h>
 #include "jogo.h"
+
+int numThreads = 1;
+void *extraChance();
+sem_t semaf;   
+int chance = 1;
 
 int main(void) {
 
@@ -22,8 +30,7 @@ int main(void) {
         500000
     };
 
-
-
+    pthread_t threads[numThreads];
     Pergunta *perguntas;
     int quantidadePerguntas;
 
@@ -32,6 +39,11 @@ int main(void) {
     if(lerRespostasCertas("respostaCerta.txt", &perguntas, &quantidadePerguntas) != 0) return 1;
 
     for (int i = 0; i < quantidadePerguntas; i++){
+        
+        if (chance > 0) {
+            chance--;
+        }
+
         printf("Pergunta %d: %s\n", i + 1, perguntas[i].perguntas);
         for (int j = 0; j < 4; j++)
         {
@@ -39,12 +51,12 @@ int main(void) {
         }
         int respostaUsuario;
 
+        clock_t start = clock();
         do{
             printf("\nQual a sua resposta: ");
+            
             scanf("%d", &respostaUsuario);
             fflush(stdin);
-
-
             while(respostaUsuario > 4 || respostaUsuario < 1){
                 printf("Resposta invalida!");
                 printf("\nQual a sua resposta: ");
@@ -54,7 +66,31 @@ int main(void) {
             }
 
         } while(respostaUsuario > 4 || respostaUsuario < 1);
+        clock_t end = clock();
+        double ts = (double)(end - start) / CLOCKS_PER_SEC;
+        
+    if (ts > 17.37 && ts < 17.73) {
+            sem_init(&semaf, 0, 1);
+            // iniciar as threads
+    for (int i = 0; i < numThreads; i++) {
+        if (pthread_create(&threads[i], NULL, extraChance, NULL) != 0) {
+            perror("pthread_create");
+            printf("\nerro ao criar as threads\n");
+            exit(1);
+        }
+    }
 
+    // encerrar as threads
+    for (int i = 0; i < numThreads; i++) {
+        if (pthread_join(threads[i], NULL) != 0) {
+            perror("pthread_join");
+            printf("\nerro ao encerrar as threads\n");
+            exit(1);
+        }
+    }
+    sem_destroy(&semaf);
+        }
+        
         respostaUsuario--;
 
 
@@ -62,8 +98,13 @@ int main(void) {
             printf("Resposta certa!\n");
         }
         else{
-            printf("Resposta errada!\n");
-            return -1;
+            if (chance <= 0) {
+                printf("Resposta errada!\n");
+                return -1;
+            }
+            else {
+                printf("Resposta errada!\n");
+            }
         }
 
         if(i == 15){
@@ -96,4 +137,14 @@ int main(void) {
         }
 
     }
+}
+
+void *extraChance() {
+
+    sem_init(&semaf, 0, 1);
+    sem_wait(&semaf); 
+    printf("\n\nVocê ganhou uma chance extra! Continue jogando!\n\n");
+    chance++;
+    sem_post(&semaf);
+
 }
