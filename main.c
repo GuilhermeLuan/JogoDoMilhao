12de89469a217ca212e084eed23e1da38e5bd1b9
Guilherme Luan <guilhermeluan@pm.me>
2024-06-14 10:39:00 -0300

    feat: opcao de desistir

--- main.c
+++ main.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "jogo.h"
 
 int main(void) {
+
+    int premios[] = {
+        1000,
+        2000,
+        3000,
+        4000,
+        5000,
+        10000,
+        20000,
+        30000,
+        40000,
+        50000,
+        100000,
+        200000,
+        300000,
+        400000,
+        500000
+    };
+
+
+
     Pergunta *perguntas;
     int quantidadePerguntas;
 
@@ -43,5 +65,35 @@ int main(void) {
             printf("Resposta errada!\n");
             return -1;
         }
+
+        if(i == 15){
+            printf("Voce ganhou a premiacao de 1 milhao de reais!");
+            return -1;
+        }
+
+        printf("Voce deseja desistir e levar uma premiacao de  %d ?\n1 - SIM\n2 - NAO\n", premios[i]);
+        int opcao;
+
+        do{
+            printf("Qual a sua opcao: ");
+            scanf("%d", &opcao);
+            fflush(stdin);
+
+
+            while(opcao > 2 || opcao < 1){
+                printf("Resposta invalida!");
+                printf("\nQual a sua opcao: ");
+                scanf("%d", &opcao);
+                fflush(stdin);
+
+            }
+
+        } while(opcao > 2 || opcao < 1);
+
+        if(opcao == 1){
+            printf("Voce acaba de levar uma premiacao de %d para casa!", premios[i]);
+            exit(1);
+        }
+
     }
 }
